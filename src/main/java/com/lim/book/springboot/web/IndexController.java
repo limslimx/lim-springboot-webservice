112c41d2c2aae855a6977b152b3f4dde75ff5cf9
saemyunglim <saemyung2000@naver.com>
2020-03-30 15:03:27 +0900

    스프링 시큐리티를 이용한 구글, 네이버 소셜 로그인 기능 구현

--- src/main/java/com/lim/book/springboot/web/IndexController.java
+++ src/main/java/com/lim/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.lim.book.springboot.web;
 
+import com.lim.book.springboot.config.auth.LoginUser;
+import com.lim.book.springboot.config.auth.dto.SessionUser;
 import com.lim.book.springboot.service.PostsService;
 import com.lim.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String Index(Model model){
+    public String Index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user!=null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
